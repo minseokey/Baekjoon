fe312399bfe3d5344e35f18fda787f9c84a43f4d
minseokey <91869302+minseokey@users.noreply.github.com>
2022-09-01 01:17:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,12 @@
+num = int(input())
+lis = [0]*(num+1)
+lis[0] = 1
+lis[1] = 3
+
+if num == 1:
+    print(1)
+else:
+    for i in range(2,num+1):
+        lis[i] = (lis[i-1] + (2*lis[i-2]))%10007
+
+    print(lis[num-1])
