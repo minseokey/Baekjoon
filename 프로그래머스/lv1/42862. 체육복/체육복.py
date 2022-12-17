9843b76bd128f5c16c91c9c653a7795a83ce911d
minseokey <91869302+minseokey@users.noreply.github.com>
2022-12-17 22:05:33 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,10 +1,29 @@
+import copy
+
+
+lis = []
+def recur(reserve, lostindex, lost, count):
+
+    if lostindex == len(lost):
+        lis.append(count)
+        return
+
+    if lost[lostindex] in reserve:
+        newreserve1 = copy.deepcopy(reserve)
+        newreserve1.remove(lost[lostindex])
+        recur(newreserve1,lostindex + 1,lost,count+1)
+    if lost[lostindex]-1 in reserve:
+        newreserve2 = copy.deepcopy(reserve)
+        newreserve2.remove(lost[lostindex] - 1)
+        recur(newreserve2,lostindex + 1,lost,count+1)
+    if lost[lostindex]+1 in reserve:
+        newreserve3 = copy.deepcopy(reserve)
+        newreserve3.remove(lost[lostindex] + 1)
+        recur(newreserve3,lostindex + 1,lost,count+1)
+    else:
+        recur(reserve,lostindex+1,lost,count)
+
 def solution(n, lost, reserve):
-    setlost = list(set(lost) - set(reserve))
-    setreserve = list(set(reserve) - set(lost))
-    for i in range(len(setlost)):
-        if setlost[i] -1 in setreserve:
-            setreserve.remove(setlost[i]-1)
-        elif setlost[i] + 1 in setreserve:
-            setreserve.remove(setlost[i]+1)
-            
-    return n - (len(lost) - (len(reserve) - len(setreserve)))
+
+    recur(reserve,0,lost,0)
+    return n - (len(lost) - max(lis))
