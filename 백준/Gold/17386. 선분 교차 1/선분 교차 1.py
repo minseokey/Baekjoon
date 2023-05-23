69649bd57c00472e6fe0044c6a88018b3992d0fc
minseokey <91869302+minseokey@users.noreply.github.com>
2023-05-23 21:48:14 +0900

    [Gold III] Title: 선분 교차 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,48 @@
+import sys
+
+# 1번 점
+Ax, Ay, Bx, By = map(int, sys.stdin.readline().split())
+# 2번 점
+Cx, Cy, Dx, Dy = map(int, sys.stdin.readline().split())
+
+A = [Ax, Ay]
+B = [Bx, By]
+C = [Cx, Cy]
+D = [Dx, Dy]
+
+line1 = [A, B]
+line2 = [C, D]
+
+
+# -1 => 반시계
+# 1 => 시계
+# 0 => 평행
+def ccw(A, B, C):
+    a1, a2 = A[0], A[1]
+    b1, b2 = B[0], B[1]
+    c1, c2 = C[0], C[1]
+
+    sl = (a1 * b2 + b1 * c2 + c1 * a2) - (a2 * b1 + b2 * c1 + c2 * a1)
+    if sl < 0:
+        return -1
+    elif sl > 0:
+        return 1
+    else:
+        return 0
+
+
+# 1번 라인 중심
+ABC = ccw(A, B, C)
+ABD = ccw(A, B, D)
+
+# 2번 라인 중심
+CDA = ccw(C, D, A)
+CDB = ccw(C, D, B)
+
+# 1번 선 AB, 2번 선  CD
+# if ABC * ABD == 0 and CDA * CDB == 0:
+    
+if ABC * ABD <= 0 and CDA * CDB <= 0:
+    print(1)
+else:
+    print(0)
