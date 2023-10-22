7fdfb059c471c2ba3d31550d7deaa5616fa2eb8e
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2023-10-23 01:11:46 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,14 @@
 import sys
-n,m = map(int,sys.stdin.readline().split())
 
-coin = [0] * n
-for i in range(n):
-    coin[i]=(int(sys.stdin.readline()))
-num = 0
+def coin(now):
+    ans = 0
+    while now > 0:
+        tt = c.pop()
+        ans += (now // tt)
+        now %= tt
+    return ans
 
-for i in reversed(coin):
-    num += int(m/i)
-    m %= i
-    if m == 0:
-        break
-print(num)
+minimum = float('inf')
+n,k = map(int,sys.stdin.readline().split())
+c = [int(sys.stdin.readline()) for _ in range(n)]
+print(coin(k))
