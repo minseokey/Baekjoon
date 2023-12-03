9eef28546079b59c4dec147d6d3f7e3809682c88
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2023-12-04 07:46:44 +0900

    [Platinum V] Title: 등산 마니아, Time: 1852 ms, Memory: 144500 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/20188. 등산 마니아/등산 마니아.py
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.py
@@ -1,27 +1,25 @@
 import sys
-
-
-def dfs(cur):
-    global ans
-    d[cur] = 1
-    for i in v[cur]:
-        if not d[i]:
-            d[cur] += dfs(i)
-    ans += ret(N) - ret(N - d[cur])
-    return d[cur]
-
-def ret(n):
-    return n * (n - 1) // 2
-
+sys.setrecursionlimit(10 ** 6)
 N = int(sys.stdin.readline())
-v = [[] for _ in range(N + 1)]
-d = [0] * (N + 1)
+v = [[] for _ in range(N+1)]
+d = [0] * (N+1)
 ans = 0
-
+NN = N * (N - 1) // 2
 for _ in range(N - 1):
     a, b = map(int, sys.stdin.readline().split())
     v[a].append(b)
     v[b].append(a)
 
-dfs(1)
-print(ans - ret(N))
+
+def dfs(cur, pre):
+    global ans
+    d[cur] = 1
+    for i in v[cur]:
+        if i != pre:
+            d[cur] += dfs(i, cur)
+    ans += NN - ((N - d[cur]) * (N - d[cur] - 1) // 2)
+    return d[cur]
+
+
+dfs(1,0)
+print(ans - NN)
