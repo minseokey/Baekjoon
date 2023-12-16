7b53a403bd94e4296d47736281880c42d296213d
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2023-12-17 06:13:04 +0900

    [Gold V] Title: 평범한 배낭, Time: 4452 ms, Memory: 227036 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,23 +1,13 @@
-n, k = map(int, input().split())  # 물건의 수 n과 배낭의 용량 k 입력 받기
+import sys
 
-w = [0]  # 물건의 무게를 저장할 리스트
-v = [0]  # 물건의 가치를 저장할 리스트
-
-for i in range(n):
-    a, b = map(int, input().split())  # 각 물건의 무게와 가치 입력 받기
-    w.append(a)
-    v.append(b)
-
-# 2차원 배열 dp 생성하기
-dp = [[0] * (k+1) for _ in range(n+1)]
-
-# dp 배열 채우기
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        if j >= w[i]:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i])
+n, k = map(int, sys.stdin.readline().split())
+arr = [[0] * (k + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    size, value = map(int, sys.stdin.readline().split())
+    for j in range(1, k + 1):
+        if j - size >= 0:
+            arr[i][j] = max(arr[i - 1][j], arr[i-1][j - size] + value)
         else:
-            dp[i][j] = dp[i-1][j]
+            arr[i][j] = arr[i - 1][j]
 
-# 배낭에 넣을 수 있는 물건들의 가치 합의 최댓값 출력하기
-print(dp[n][k])
+print(arr[n][k])
