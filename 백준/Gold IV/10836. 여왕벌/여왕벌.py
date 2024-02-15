80862826bea27c64facffc14bdb1595e62e1c20f
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-02-15 10:47:17 +0900

    [Gold IV] Title: 여왕벌, Time: 7852 ms, Memory: 117120 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold IV/10836. 여왕벌/여왕벌.py
+++ 백준/Gold IV/10836. 여왕벌/여왕벌.py
@@ -3,29 +3,28 @@ import sys
 m, n = map(int, sys.stdin.readline().split())
 beewax = [[1] *m for _ in range(m)]
 
-# 3 -> [0,2],[0,1],[0,0],[1,0],[2,0]
-
-ADDLIS = []
+Ay = []
+Ax = []
 for i in range(m - 1, 0, -1):
-    ADDLIS.append((i, 0))
+    Ay.append(i)
+    Ax.append(0)
 for i in range(m):
-    ADDLIS.append((0, i))
+    Ay.append(0)
+    Ax.append(i)
 
+# 유일한 이중루프
 for _ in range(n):
-    groth = list(map(int, sys.stdin.readline().split()))  # 0개수 1개수 2개수
-    A_ind = 0
-    A_ind += groth[0]
-    for i in range(groth[1]):
-        beewax[ADDLIS[A_ind][0]][ADDLIS[A_ind][1]] += 1
-        A_ind += 1
-    for i in range(groth[2]):
-        beewax[ADDLIS[A_ind][0]][ADDLIS[A_ind][1]] += 2
-        A_ind += 1
+    z,o,t = list(map(int, sys.stdin.readline().split()))  # 0개수 1개수 2개수
+
+    for i in range(z, z+o):
+        beewax[Ay[i]][Ax[i]] += 1
+    for i in range(z+o, z+o+t):
+        beewax[Ay[i]][Ax[i]] += 2
 
 for i in range(1, m):
     for j in range(1, m):
-        temp = max(beewax[i - 1][j], beewax[i - 1][j - 1], beewax[i][j - 1])
-        beewax[i][j] = temp
+        beewax[j][i] = beewax[0][i]
 
 for j in beewax:
     print(*j)
+
