7d1dd566882abd23d484afd1dfb106d481e590e8
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-02-18 13:03:59 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,9 +1,20 @@
-foi = int(input())
-for w in range(foi):
-    FlooR = int(input())
-    NuM = int(input())
-    NumlisT = [q for q in range(1, NuM+1)]
-    for i in range(FlooR):
-        for j in range(1, NuM):
-            NumlisT[j] = NumlisT[j] + NumlisT[j - 1]
-    print(NumlisT[NuM - 1])
+import sys
+
+t = int(sys.stdin.readline())
+
+dp = [[0] * (15) for _ in range(15)]
+
+# init
+for i in range(len(dp[0])):
+    dp[0][i] = i
+
+# dp
+for i in range(1,15):
+    for j in range(1,15):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+for _ in range(t):
+    k = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
+
+    print(dp[k][n])
