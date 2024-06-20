a817f3512defaef56cd303eb77f3e03273b60585
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-06-20 17:19:23 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,32 @@
 import sys
 
-sys.setrecursionlimit(5000)
-n = int(input())
+t = int(sys.stdin.readline().strip())
+DIR = [(0,1), (-1,0), (1,0), (0,-1)]
 
-for i in range(n):
-    maxx,maxy,num = map(int,sys.stdin.readline().split())
-    lisorigin = [[0 for j in range(maxx)]for k in range(maxy)]
-    liscopy = [[0 for j in range(maxx)]for k in range(maxy)]
-    lis = []
-    for j in range(num):
-        pinx,piny = map(int,sys.stdin.readline().split())
-        lisorigin[piny][pinx] = 1
-        lis.append([piny,pinx])
+for _ in range(t):
+    m,n,k = map(int,sys.stdin.readline().split())
+    arr = []
+    check = []
+    for _ in range (n+1):
+        arr.append(([0])*(m+1))
+        check.append(([0]) * (m+1))
 
-    def recur(yy,xx):
-        liscopy[yy][xx] = 1
-        if xx+1 < maxx and liscopy[yy][xx+1] == 0 and lisorigin[yy][xx+1] == 1:
-            recur(yy, xx + 1)
-        if xx > 0 and liscopy[yy][xx-1] == 0 and lisorigin[yy][xx-1] == 1:
-            recur(yy, xx - 1)
-        if yy+1 < maxy and liscopy[yy+1][xx] == 0 and lisorigin[yy+1][xx] == 1:
-            recur(yy + 1, xx)
-        if yy > 0 and liscopy[yy-1][xx] == 0 and lisorigin[yy-1][xx] == 1:
-            recur(yy - 1, xx)
-        else:
-            return
+    for _ in range(k):
+        c,r = map(int,sys.stdin.readline().split())
+        arr[r][c] = 1
 
-    bug = 0
-    for i in lis:
-        if liscopy[i[0]][i[1]] == 0:
-            recur(i[0],i[1])
-            bug += 1
-    print(bug)
+    count = 0
+    for i in range(n+1):
+        for j in range(m+1):
+            # BFS
+            if arr[i][j] == 1 and check[i][j] == 0:
+                count += 1
+                queue = [(i,j)]
+                while queue:
+                    ty, tx = queue.pop()
+                    check[ty][tx] = 1
+                    for dy,dx in DIR:
+                        if arr[ty+dy][tx+dx] == 1 and check[ty+dy][tx+dx] == 0:
+                            queue.append((ty+dy, tx+dx))
+
+    print(count)
