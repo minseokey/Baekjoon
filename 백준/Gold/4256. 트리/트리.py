182eee0b50271891ee3e07e250f58a143942319c
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-06-24 01:23:21 +0900

    [Gold II] Title: 트리, Time: 732 ms, Memory: 31260 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.py
+++ 백준/Gold/4256. 트리/트리.py
@@ -1,20 +1,26 @@
 import sys
 
-t = int(sys.stdin.readline())
-for _ in range(t):
+fois = int(sys.stdin.readline())
+for _ in range(fois):
     n = int(sys.stdin.readline())
-    preord = list(map(int, sys.stdin.readline().split()))
-    inord = list(map(int, sys.stdin.readline().split()))
-    postord = []
+    preord = list(map(int,sys.stdin.readline().split()))
+    inord = list(map(int,sys.stdin.readline().split()))
+    ans = []
 
+    # 범위 지정이 중요
+    def recur(p_index, i_s, i_e):
 
-    def recur(root, in_s, in_e):
-        for i in range(in_s, in_e):
-            if inord[i] == preord[root]:
-                recur(root + 1, in_s, i)
-                recur(root + 1 + i - in_s, i + 1, in_e)
-                postord.append(preord[root])
+        if i_s < i_e:
+            # p의 시작점이 루트다.
+            root = preord[p_index]
+            in_index = inord.index(root)
+            if i_s <= in_index < i_e:
+                recur(p_index + 1, i_s, in_index)
+            if i_s <= in_index < i_e:
+                recur(p_index + 1 + in_index - i_s, in_index + 1, i_e)
+            #더하기
+            ans.append(root)
 
+    recur(0,0,n)
 
-    recur(0, 0, n)
-    print(*postord)
+    print(*ans)
