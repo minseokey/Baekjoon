d8e24818104e0b624f8030e661bf0dd4558d3d1e
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-06-28 20:14:19 +0900

    [Bronze I] Title: 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -2,10 +2,4 @@ import sys
 
 n = int(sys.stdin.readline())
 lis = list(map(int,sys.stdin.readline().split()))
-
-lis.sort()
-
-if len(lis) % 2 == 1: # 홀수
-    print(lis[len(lis)//2] ** 2)
-else:
-    print(lis[len(lis)//2 - 1] * lis[len(lis)//2])
+print(max(lis) * min(lis))
