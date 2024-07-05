3de86c36140c3d6e7fcdf5857289f0d4109b95ed
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-07-05 18:08:01 +0900

    [Silver II] Title: DFS와 BFS, Time: 80 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,49 +1,59 @@
 import sys
-from collections import deque
+from collections import defaultdict, deque
 
-vertex = dict()
 n,m,v = map(int,sys.stdin.readline().split())
+
+# 처음에 길 그래프 만들기
+road = defaultdict(list)
 for i in range(m):
-    tempa,tempb = map(int,sys.stdin.readline().split())
-    if tempa in vertex.keys():
-        vertex[tempa].append(tempb)
-    else:
-        vertex[tempa] = [tempb]
-    if tempb in vertex.keys():
-        vertex[tempb].append(tempa)
-    else:
-        vertex[tempb] = [tempa]
+    s,e = map(int, sys.stdin.readline().split())
+    road[s].append(e)
+    road[e].append(s)
+
+# 각각의 연결가능한 지점 정렬하기
+for i in range(1, n + 1):
+    if i in road.keys():
+        road[i].sort()
+
+# 이미 방문한 노드 처리하기
+v_dfs = [False] * (n+1)
+v_bfs = [False] * (n+1)
+
+# dfs
+d_queue = deque([v])
+d_ans = []
+
+while d_queue:
+    now = d_queue.pop()  # dfs
+    
+    # 방문처리
+    if not v_dfs[now]:
+        d_ans.append(now)
+        v_dfs[now] = True
+
+        if now in road.keys():
+            # 다음 이동경로 -> 반대로 들어가야함
+            for nex in road[now][::-1]:
+                if not v_dfs[nex]:
+                    d_queue.append(nex)
 
 
-checkdfs = [False for i in range(n)]
-dfsans = []
+# bfs
+b_queue = deque([v])
+b_ans = []
+while b_queue:
+    now = b_queue.popleft()  # bfs
+    
+    # 방문처리
+    if not v_bfs[now]:
+        b_ans.append(now)
+        v_bfs[now] = True
+    
+        # 다음 이동 경로
+        if now in road.keys():
+            for nex in road[now]:
+                if not v_bfs[nex]:
+                    b_queue.append(nex)
 
-def dfs(check, now):
-    dfsans.append(now)
-    check[now-1] = True
-    if now in vertex.keys():
-        for i in sorted(vertex[now]):
-            if not check[i-1]:
-                dfs(check,i)
-    else:
-        return
-
-
-dfs(checkdfs,v)
-print(*dfsans)
-
-queue = deque()
-queue.append(v)
-bfsans = []
-checkbfs = [False for i in range(n)]
-while queue:
-    temp = queue.popleft()
-    if not checkbfs[temp - 1]:
-        bfsans.append(temp)
-    checkbfs[temp - 1] = True
-    if temp in vertex.keys():
-        for i in sorted(vertex[temp]):
-            if not checkbfs[i - 1]:
-                queue.append(i)
-
-print(*bfsans)
+print(*d_ans)
+print(*b_ans)
