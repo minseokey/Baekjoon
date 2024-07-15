5608127d4264bd3ef726f90c9fbc05a30b021fd4
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2024-07-15 15:48:09 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 416 ms, Memory: 236352 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -1,17 +1,9 @@
-import sys
-
-lis1 = list(sys.stdin.readline().strip())
-lis2 = list(sys.stdin.readline().strip())
-dp = [[0] * (len(lis2) + 1) for _ in range(len(lis1) + 1)]
-
-for i in range(len(lis1)):
-    for j in range(len(lis2)):
-        if lis1[i] == lis2[j]:
-            dp[i+1][j+1] = dp[i][j] + 1
-            
-maxx = 0
-
-for i in dp:
-    maxx = max(maxx, max(i))
-
-print(maxx)
+a=' '+input()
+b=' '+input()
+n,m=len(a),len(b)
+d=[[0]*m for _ in range(n)]
+for i in range(1,n):
+    for j in range(1,m):
+        if a[i]==b[j]:
+            d[i][j] = d[i-1][j-1]+1
+print(max(map(max, d)))
