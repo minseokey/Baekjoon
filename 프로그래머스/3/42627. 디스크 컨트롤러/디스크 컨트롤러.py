cb67211bf19cf4cacffaa1398487bb5b1bb04cfe
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-17 20:25:40 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.98 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,28 +1,3 @@
-import heapq
-from collections import deque
-
 def solution(jobs):
-    # jobs -> 요청 시간, 작업 시간.
-    jobs = deque(sorted(jobs))
-    on_sche = []
-    len_j = len(jobs)
-    cnt = 0
-    ans = 0
-    
-    while jobs or on_sche:
-        # 타이밍 맞는 jobs 추가.
-        while jobs:
-            if cnt >= jobs[0][0]: 
-                heapq.heappush(on_sche, tuple(reversed(jobs.popleft())))
-            else:
-                break
-
-        # 가장 짧은 일 수행.
-        if on_sche:
-            tmp = heapq.heappop(on_sche)
-            cnt += tmp[0]
-            ans += cnt - tmp[1]
-        else:
-            cnt += 1
-    
-    return ans//len_j
+    answer = 0
+    return answer
