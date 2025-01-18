ed02a32902e264ca3828bbde8e8f6a1ef05fb64b
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-19 01:04:08 +0900

    [level 2] Title: 소수 찾기, Time: 4457.97 ms, Memory: 397 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,25 +1,6 @@
-import itertools
-
-
 def solution(numbers):
-    all_length = [i for i in range(10 ** len(numbers) + 1)]
-    ans = 0
-    all_length[1] = 0
-    for i in range(2, len(all_length)):
-        if all_length[i] != 0:
-            for j in range(i + i,len(all_length), i):
-                all_length[j] = 0
-                    
-    tmp = []
-    for i in range(1, len(numbers) + 1):
-        for i in list(itertools.permutations(numbers,i)):
-            if all_length[(int("".join(i)))] != 0:
-                all_length[(int("".join(i)))] = 0
-                ans += 1
-    
-    
-    return ans
-            
-    
-    
-            
+    sosu = [2,3,5]
+    all_length = [i+1 for i in range(10 ** len(numbers) + 1)]
+    print(all_length)
+    # for i in range()
+    # return answer
