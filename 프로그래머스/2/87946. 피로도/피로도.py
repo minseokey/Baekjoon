4bde61b8cdd5c0f0f830181e574d187e7397687a
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-21 02:40:28 +0900

    [level 2] Title: 피로도, Time: 34.63 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,16 +1,3 @@
 def solution(k, dungeons):
-    
-    ans = 0
-    
-    def recur(cnt, now_k):
-        nonlocal ans
-        ans = max(cnt, ans)
-        for i in range(len(dungeons)):
-            if now_k >= dungeons[i][0]:
-                tmp = dungeons.pop(i)
-                recur(cnt+1, now_k - tmp[1] )
-                dungeons.insert(i,tmp)
-    
-    recur(0,k)
-    
-    return ans
+    answer = -1
+    return answer
