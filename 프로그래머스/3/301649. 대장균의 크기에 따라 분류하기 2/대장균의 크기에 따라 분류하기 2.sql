a9062867a3e82983fb7d7adf0cdf5327f061588d
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-28 23:46:22 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,26 +1,8 @@
-with total_count as (
-    select count(*) as cnt
-    from ecoli_data
-)
-
-select ID, 
-       case 
-           when size_of_colony <= (select max(size_of_colony)
-                                   from ecoli_data
-                                   order by size_of_colony
-                                   limit 1 offset (select cnt / 4 - 1 from total_count)
-                                  ) then 'LOW'
-           when size_of_colony <= (select max(size_of_colony)
-                                   from ecoli_data
-                                   order by size_of_colony
-                                   limit 1 offset (select cnt / 2 - 1 from total_count)
-                                  ) then 'MEDIUM'
-           when size_of_colony <= (select max(size_of_colony)
-                                   from ecoli_data
-                                   order by size_of_colony
-                                   limit 1 offset (select cnt / 4 * 3 - 1 from total_count)
-                                  ) then 'HIGH'
-           else 'CRITICAL'
-       end as colony_name
+select ID, CASE
+        when ntile(4) over(order by SIZE_OF_COLONY DESC) = 1 then 'CRITICAL'
+        when ntile(4) over(order by SIZE_OF_COLONY DESC) = 2 then 'HIGH'
+        when ntile(4) over(order by SIZE_OF_COLONY DESC) = 3 then 'MEDIUM'
+        when ntile(4) over(order by SIZE_OF_COLONY DESC) = 4 then 'LOW'
+    END as COLONY_NAME
 from ecoli_data
-order by id;
+order by id
