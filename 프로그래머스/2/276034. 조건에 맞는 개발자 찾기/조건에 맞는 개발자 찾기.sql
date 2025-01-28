633de4671cc0f673f7bbc9fb3a7a953b1aeb56cc
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-28 22:38:25 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,4 +1,4 @@
-select distinct(id) ,EMAIL,FIRST_NAME,LAST_NAME
-from skillcodes join developers
-where skill_code & CODE = CODE and name in ("Python","C#")
+-- 코드를 작성해주세요
+select distinct ID,EMAIL,FIRST_NAME,LAST_NAME
+from developers as d join skillcodes as s on (d.SKILL_CODE & s.code) = s.code and s.Name in ("C#","Python")
 order by id
