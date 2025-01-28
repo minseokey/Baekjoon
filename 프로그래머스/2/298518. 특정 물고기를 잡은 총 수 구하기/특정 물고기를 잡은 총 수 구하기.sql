2d1193ebeea4b3e040bb921574968f029b418b68
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-28 22:44:11 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++ 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,4 +1,5 @@
--- 코드를 작성해주세
+-- 코드를 작성해주세요
 select count(*) as fish_count
 from fish_info join fish_name_info using(fish_type)
-where fish_name in ("BASS","SNAPPER")
+
+where fish_name in ("BASS", "SNAPPER")
