1a5b1822cc04305210cbb2caebed4ffad8d53f8f
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-01-29 00:12:22 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,7 +1,5 @@
 -- 코드를 작성해주세요
 select c.id
-from ecoli_data a left join ecoli_data b on a.id = b.parent_id left join ecoli_data c on b.id = c.parent_id
-where c.id is not null and a.parent_id is null
+from ecoli_data as g join ecoli_data as p on g.id = p.parent_id and g.parent_id is null join ecoli_data as c on p.id = c.parent_id
+where c.id is not null
 order by c.id
-
-
