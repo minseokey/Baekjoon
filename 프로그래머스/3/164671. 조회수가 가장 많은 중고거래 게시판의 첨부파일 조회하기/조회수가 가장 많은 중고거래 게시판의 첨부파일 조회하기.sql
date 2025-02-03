3df1c1d118452f7b5f5b48fff0022b889297c9f1
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-02-04 00:44:27 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,11 +1,7 @@
 -- 코드를 입력하세요
-SELECT concat("/home/grep/src/" , board_id, "/" , file_id , file_name ,  File_ext) as FILE_PATH
-from USED_GOODS_BOARD join USED_GOODS_FILE using(Board_Id)
-where Board_id = (select Board_Id
-                    from used_goods_board
-                    order by views desc limit 1)
+SELECT  concat("/home/grep/src/",Board_id, "/",file_id,file_name,file_ext) as file_path
+from USED_GOODS_BOARD join USED_GOODS_FILE using(Board_id)
+where views = (select max(views)
+               from USED_GOODS_BOARD)
+
 order by file_id desc
-
-# select *
-# from USED_GOODS_BOARD
-
