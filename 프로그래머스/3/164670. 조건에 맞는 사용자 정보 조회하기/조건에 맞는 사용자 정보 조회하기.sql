fb5bed9f682edbd3f2926da8f953e90b89c21e6a
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-02-04 19:32:51 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,12 +1,6 @@
 -- 코드를 입력하세요
-SELECT USED_GOODS_USER.USER_ID,USED_GOODS_USER.NICKNAME,
-    concat(USED_GOODS_USER.city," ",USED_GOODS_USER.street_address1," ",USED_GOODS_USER.street_address2) as 전체주소,
-    concat(substring(USED_GOODS_USER.tlno,1,3) ,"-",substring(USED_GOODS_USER.tlno,4,4), "-", substring(USED_GOODS_USER.tlno,8,4))  as 전화번호
-
-from USED_GOODS_BOARD join USED_GOODS_USER on USED_GOODS_USER.user_id = USED_GOODS_BOARD.writer_id
-
-group by USED_GOODS_USER.user_id
-
-having count(USED_GOODS_BOARD.writer_id) >= 3
-
-order by USED_GOODS_USER.user_id desc
+SELECT USER_ID, NICKNAME, concat(city, " ",STREET_ADDRESS1, " ", STREET_ADDRESS2) 전체주소, concat(substr(tlno,1,3),"-",substr(tlno,4,4),"-",substr(tlno,8,4)) as 전화번호
+from USED_GOODS_BOARD B join USED_GOODS_USER U on B.writer_id = U.user_id
+group by B.Writer_id
+having count(B.writer_id) >= 3
+order by user_id desc
