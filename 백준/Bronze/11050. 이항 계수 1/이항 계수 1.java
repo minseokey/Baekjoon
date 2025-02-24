b22f09cd337da2ca0464f2a7196bc48679ec661d
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-02-24 15:30:08 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,32 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException; 
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
+public class Main{
+    static int a,b;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] tmp = br.readLine().split(" ");
+        a = Integer.parseInt(tmp[0]);
+        b = Integer.parseInt(tmp[1]);
+        b = Math.min(b, a-b);
 
+        int top = 1;
+        int bot = 1;
 
-public class Main {
-	public static void main(String[] args)throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] inp = br.readLine().split(" ");
-		int big = Integer.parseInt(inp[0]);
-		int sma = Integer.parseInt(inp[1]);
-		int bigg = 1;
-		int smaa = 1;
-		while(sma != 0){
-			bigg *= big;
-			big--;
-			smaa *= sma;
-			sma--;
-		}
-		
-		System.out.print(bigg/smaa);
-
-		bw.flush();
-		bw.close();
-	}
+        for(int i = 1; i <= b; i++){
+            bot *= i;
+            top *= a+1-i;
+        }
+        bw.write(String.valueOf(top/bot));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
