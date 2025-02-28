02510edae9a2a569a9bbe09d2672c6437c45ecc0
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-02-28 10:01:21 +0900

    [level 2] Title: 피로도, Time: 340.93 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 34.63 ms
+메모리: 10.4 MB, 시간: 340.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 21일 02:40:26
+2025년 02월 28일 10:01:10
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,3 +1,17 @@
+import copy
+
+defa = 0
+def recur(k, dungeons, count):
+    global defa
+    for i in dungeons:
+        if k >= i[0]:
+            copyden = copy.deepcopy(dungeons)
+            copyden.remove(i)
+            recur(k - i[1], copyden, count + 1)
+    defa = max(defa, count)
+
+
 def solution(k, dungeons):
-    answer = -1
-    return answer
+    global defa
+    recur(k,dungeons,0)
+    return defa
